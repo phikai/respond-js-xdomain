9031e9cb6b952be13e40cc77d91959f944c259e6
Kai Armstrong <kai.armstrong@gmail.com>
2012-02-14 09:00:18 -0500

    added conditional IE tags

--- respond-js-xdomain-base.php
+++ respond-js-xdomain-base.php
@@ -4,13 +4,13 @@
  */
 
 function toz_rjsxd_header(){ ?>
-	<link href="<?php echo str_replace( home_url(), get_option('toz_rjsxd_external_domain'), plugin_dir_url(__FILE__)); ?>cross-domain/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
-	<link href="<?php echo str_replace( home_url(), '', plugin_dir_url(__FILE__)); ?>Respond/cross-domain/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />
+	<!--[if lt IE 9]><link href="<?php echo str_replace( home_url(), get_option('toz_rjsxd_external_domain'), plugin_dir_url(__FILE__)); ?>cross-domain/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
+	<link href="<?php echo str_replace( home_url(), '', plugin_dir_url(__FILE__)); ?>Respond/cross-domain/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" /><![endif]-->
 <?php }
 
 function toz_rjsxd_footer(){ ?>
-	<script type="text/javascript" src="<?php echo plugin_dir_url(__FILE__); ?>Respond/respond.min.js"></script>
+	<!--[if lt IE 9]><script type="text/javascript" src="<?php echo plugin_dir_url(__FILE__); ?>Respond/respond.min.js"></script>
 	<link href="<?php echo str_replace( home_url(), '', plugin_dir_url(__FILE__)); ?>Respond/cross-domain/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />
-	<script type="text/javascript" src="<?php echo str_replace( home_url(), '', plugin_dir_url(__FILE__)); ?>Respond/cross-domain/respond.proxy.js"></script>
+	<script type="text/javascript" src="<?php echo str_replace( home_url(), '', plugin_dir_url(__FILE__)); ?>Respond/cross-domain/respond.proxy.js"></script><![endif]-->
 <?php }
 ?>
