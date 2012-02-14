236b1fa4654b01176bbf4a287f035390beb15ab5
Kai Armstrong <kai.armstrong@gmail.com>
2012-02-14 08:36:18 -0500

    file order adjustment in complete load

--- respond-js-xdomain-base.php
+++ respond-js-xdomain-base.php
@@ -5,6 +5,7 @@
 
 function toz_rjsxd_header(){ ?>
 	<link href="<?php echo str_replace( home_url(), get_option('toz_rjsxd_external_domain'), plugin_dir_url(__FILE__)); ?>cross-domain/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
+	<link href="<?php echo str_replace( home_url(), '', plugin_dir_url(__FILE__)); ?>cross-domain/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />
 <?php }
 
 function toz_rjsxd_footer(){ ?>
--- respond-js-xdomain.php
+++ respond-js-xdomain.php
@@ -9,8 +9,8 @@ Author URI: http://kaiarmstrong.com
 */
 
 if ( @include_once('respond-js-xdomain-base.php') ) {
-	add_action('wp_head','toz_rjsxd_header', 1);
-	add_action('wp_footer','toz_rjsxd_footer', 1);
+	add_action('wp_head','toz_rjsxd_header');
+	add_action('wp_footer','toz_rjsxd_footer');
 }
 
 /********** WordPress Administrative ********/
